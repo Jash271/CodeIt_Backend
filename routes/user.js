f4969f2a72383732def25420640abf678774afe8
Jash271 <shahjash271@gmail.com>
2020-09-27 12:13:28 +0530

    file updates

--- routes/user.js
+++ routes/user.js
@@ -92,6 +92,56 @@ router.post('/register', upload.single('imageUrl'), async (req, res) => {
   }
 });
 
+router.post('/user_register', async (req, res) => {
+  const url = req.protocol + '://' + req.get('host');
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        msg: 'User already exists',
+      });
+    }
+    console.log(req);
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {
+        expiresIn: 360000,
+      },
+      (err, token) => {
+        if (err) throw err;
+
+        res.json({ token, user });
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 //Logging in the already registered user
 router.post(
   '/login',
